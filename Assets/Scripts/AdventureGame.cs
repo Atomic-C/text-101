f2dba8ad2f45ddcbe157751ad89817180445dc12
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-07 23:55:12 +0000

    Pedro fixes IndexOutOfRange Exception with a for loop solution

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -31,18 +31,27 @@ public class AdventureGame : MonoBehaviour
     {
         State[] nextStates = state.GetNextState();
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        for (int i = 0; i < nextStates.Length; i++)
         {
-            state = nextStates[0];
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
+
+
+        /*        if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    state = nextStates[0];
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    state = nextStates[1];
+                }
+                else if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    state = nextStates[2];
+                }*/
         textComponent.text = state.GetStateStory();
         textComponentImproved.text = state.GetStateStory();
     }
